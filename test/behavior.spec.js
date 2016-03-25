9d3ce038766e99fbcffc5a5628ab979a3a192a98
Pamela Yang <PamelaYang@Pamelas-MacBook-Pro.local>
2016-03-24 14:58:54 -1000

    one change

---
+++ test/behavior.spec.js
@@ -0,0 +1,14 @@
+var chai = require('chai'); // pulls in the 'chai' package installed through NPM
+var expect = chai.expect;   // this is how we use chai's `expect` assertion
+chai.should();              // this is how we use chai's `should` assertion
+
+var fileToTest = require('../path/to/file');
+
+/* all your tests will be written below */
+
+// this describe and it block is our 'hello world', we will replace this later with
+describe('Test Suite', function(){
+  it('should pass', function(){
+    expect(true).to.equal(true);
+  });
+});
